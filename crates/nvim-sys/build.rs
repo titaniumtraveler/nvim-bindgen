007d7d98121a1d0f79d37a64228bf285a263bae7
Jon Heinritz <jon.heinritz@protonmail.com>
2026-01-29 08:39:33 +0100

    chore: finish up rename from `nvim-codegen` -> `nvim-bindgen`

--- crates/nvim-sys/build.rs
+++ crates/nvim-sys/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    nvim_codegen::generate(
+    nvim_bindgen::generate(
         concat!(env!("CARGO_MANIFEST_DIR"), "/", "./../../vendor/neovim/"),
         &format!("{}/nvim-bindings.rs", std::env::var("OUT_DIR").unwrap(),),
     );
